c22a9b14d1946d39513dbaf1c9b6f1473679997b
daemon69 <myowngin@yandex.ru>
2025-12-29 14:15:21 +0300

    r3 :: continue spring :

--- index.js
+++ index.js
@@ -19,7 +19,26 @@ router.get('/' , async (req , res) => {
     }));
 });
 
-router.get('/test/:foo/smth/:bar' , async (req , res) => {
+router.get(
+    '/test/:foo/smth/:bar' , 
+    async (req ,res , next) => {
+        console.log('local middle ware 1');
+        next();
+    } , 
+    async (req ,res , next) => {
+        console.log('local middle ware 2');
+        next();
+    } , 
+    async (req ,res , next) => {
+        console.log('local middle ware 3');
+        
+    } , 
+    async (req ,res , next) => {
+        console.log('local middle ware 4');
+        
+    } , 
+    
+    async (req , res) => {
 
     const {method , url , params , queryParams} = req ;
 
