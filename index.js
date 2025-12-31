9c74bb7086e4276551766e366fe269c434f6c322
daemon69 <myowngin@yandex.ru>
2025-12-31 13:12:59 +0300

    r4 :: update

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ const server = http.createServer((req , res) => {
     router.handleRequest(req , res);
 });
 
+router.use(
+    (req , res , next) => {
+
+        console.log('global middleware 1');
+
+        next();
+    } ,
+    (req , res , next) => {
+
+        console.log('global middleware 1');
+
+        
+    } ,
+    (req , res , next) => {
+
+        console.log('global middleware 1');
+
+        next();
+    } ,
+);
+
+router.get('/' , (req , res  , next) => {console.log('local mw 1')} , (req , res) => {
+    console.log();
+    res.end('we stars');
+});
+
 const port = 3333;
 const host = '0.0.0.0';
 server.listen(port, host , () => {
--- router/router.js
+++ router/router.js
@@ -2,14 +2,140 @@ class Router {
 
     async handleRequest(req , res) {
 
-
+        const { url:rawURLString  , method } = req ;
         
+        const methodRoutes = this.#routes.get(method.toUpperCase());
+
+        if(methodRoutes === undefined) {
+            console.log(`trying to use method < ${method.toUpperCase()} > but this is not accepted`);
+            res.end(`method < ${method.toUpperCase()} > is not accepted`);
+            return ;
+        }
+
+        const { url , queryString } = await this.#splitURL(rawURLString);
+
+        for (const [template , routeBundle] of methodRoutes) {
+
+            const match = routeBundle.regex.exec(url);
+
+            if(match === null) continue ;
+
+
+            this.#executeMiddleware(routeBundle.middleware , req , res);
+
+            routeBundle.handler(req , res);
+            return ;
+        }        
 
         res.end('hello');
     }
 
+    async get (template , ...handlers) {
+
+        this.#addRoute(template , 'GET' , handlers);
+    }
+
+    async post (template , ...handlers) {
+
+        this.#addRoute(template , 'POST' , handlers);
+    }
+
+    async use (...middleware) {
+
+        middleware.forEach(mw => {
+            this.#middleware.push(mw);
+        });
+    }
+
+    #routes ;
+    #middleware ;
+    #acceptedMethods ;
+
+    async #executeMiddleware (middleware , req , res) {
+
+        let counter = 0 ;
+
+        const next  = () => {
+
+            const oneMiddlewareLike = middleware[counter++];
+
+            if(middleware === undefined) {
+                return 
+            }
+
+            oneMiddlewareLike(req ,res , next);
+        }
+
+        next();
+
+    }
+
+    async #splitURL (rawURLString) {
+
+        const [urlHalf , queryStringHalf] = rawURLString.split('?' , 2);
+        
+        return  {
+            url:/.+\/$/.test(urlHalf) === true ? urlHalf.replace(/\/$/ , '') : urlHalf ,
+            queryString:queryStringHalf ,
+        }
+
+    }
+
+    async #addRoute (template , method , handlers) {
+
+        const methodRoutes = this.#routes.get(method.toUpperCase());
+
+        if(methodRoutes === undefined) {
+            console.log(`this method < ${method.toUpperCase()} > is not accepted`);
+            return ;
+        }
+        
+        if(methodRoutes.has(template) === true) {
+            
+            console.log(`this template < ${method.toUpperCase()} > < ${template} > is already exist`);
+            return ;
+        }
+
+        const routeBundle = await this.#compileRouteBundle(template , handlers);
+
+        methodRoutes.set(template , routeBundle);
+
+        console.log(`added route < ${method} > < ${routeBundle.originalTemplate} >`);
+    }
+
+    async #compileRouteBundle (template , handlers) {
+        
+        const keys = [] ;
+        const regexTemplate = template.replace(/:[^\/]+/g , (_ , key) => {
+
+            keys.push(key);
+            return '([^\/]+)';
+        });
+
+        return {
+            keys , 
+            regex:new RegExp(`^${regexTemplate}$`) ,
+            handler:handlers[handlers.length - 1] , 
+            middleware:handlers.length > 1 ? handlers.slice(0 , -1) : [] , 
+            originalTemplate:template ,
+        }
+    }
+
     constructor () {
 
+        this.#middleware = [];
+
+        this.#acceptedMethods = [
+            'GET' , 'POST'
+        ];
+
+        this.#routes = new Map() ;
+
+        this.#acceptedMethods.forEach(m => {
+
+            const method = m.toUpperCase();
+            this.#routes.set(method , new Map());
+        });
     }
 }
 
