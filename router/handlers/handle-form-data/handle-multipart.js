397d697b05ab27023438b11907a16be9bc25727c
daemon69 <myowngin@yandex.ru>
2026-01-02 18:56:46 +0300

    rep--ref.handle-multipat-data.r1 :: init

--- router/handlers/handle-form-data/handle-multipart.js
+++ router/handlers/handle-form-data/handle-multipart.js
@@ -4,141 +4,36 @@ const { Readable } = require('stream');
 
 async function handleMultipart (contentTypeHeaderData , formDataBuffer) {
     
+    const boundary = contentTypeHeaderData.match(/boundarsy=(----[^;\s]+)/);
     
-    const match = /boundary=(?<boundary>----[^$\s;]+)/.exec(contentTypeHeaderData);
-
-    
-    if(match === null) {
-        
-        return 
+    if(boundary === null) {
+        return null
     }
 
-    const boundaryBuffer = Buffer.from(`--${match.groups.boundary}`);
-    const boundaryEndBuffer = Buffer.from(`--${match.groups.boundary}--`);
-    
-    const parts = await splitFormData(Buffer.from(match.groups.boundary) , formDataBuffer)
+    const parts = await _getDataBufferParts(formDataBuffer , Buffer.from(boundary[1]));
 
-    parts.pop();
-    parts.shift();
-
-    for (const part of parts) {
-
-        // if(part.equals(boundaryBuffer))
-
-        const bodySeparator = Buffer.from('\r\n\r\n');
-
-        const separatorIndex = _findIndex(part , bodySeparator);
-
-        if(separatorIndex === -1) {
-            continue ;
-        }
-
-        console.log('part part part : ' , part.toString('utf-8'));
-
-    }
-
-
-
-    // console.log(result);
+    console.log(boundary);
 
 }
 
 module.exports = handleMultipart ;
 
 
-async function saveFile (filename , data , fallback = f => f) {
-
-    try {
-        
-        const savingDir = join('.' , `${Date.now()}.${filename}`);
-        
-        const writeStriem = createWriteStream(savingDir , 'utf-8')
-        const readStream = Readable.from(data);
-
-        readStream.on('end' , () => {
-            console.log('file uploaded');
-        });
-
-        readStream.pipe(writeStriem)
-
-    }
-    catch (e) {
-
-        fallback(e)
-    }
-}
-
-async function splitFormData (separator  , buffer) {
-
-    // console.log({boundary: separator , formdata: buffer});
+async function _getDataBufferParts (dataBuffer , boundaryBuffer) {
 
     const parts = [] ;
+
     let start = 0 ;
     let index = 0 ;
 
-    while ((index = await _findIndex(buffer , separator , start)) !== -1) {
+    while () {
 
-        const part = buffer.subarray(start , index) ;
-
-        parts.push(part);
-
-        start = index + separator.length ;
-
-        if(buffer[start] === 0x0d && buffer[start + 1] === 0x0a) {
-            start += 2 ;
-        }
-
-        // console.log('part: ' , part.toString('utf-8'));
     }
 
-    parts.push(buffer.subarray(start));
-
-    // console.log('result' , parts);
-
-    return parts ;
-} 
+    return ;
+}
 
 async function _findIndex (buffer , separator , start = 0) {
 
-    for (let i = start ; i <= buffer.length - separator.length ; i++) {
-        let found = true ;
-        for (let j = 0 ; j < separator.length ; j++) {
-
-            if(buffer[i + j] !== separator[j]) {
-                found = false ;
-                break ;
-            }
-
-        }
-
-        if(found === true) {
-            return i ;
-        }
-    }
-
-    return -1 ;
 
 }
-
-function parseDispHEader (headerd) {
-
-    if(headerd === undefined) return null ;
-
-    const inputname = /name="(?<inputname>[^"]+)/.exec(headerd)?.groups.inputname ;
-    const filename = /filename="(?<filename>[^"]+)/.exec(headerd)?.groups.filename ;
-
-    return {
-        filename:filename === undefined ? null : filename ,
-        inputname: inputname === undefined ? null : inputname ,
-    }
-}
-
-function parseContentTypeHeaderData (headerd) {
-
-    if(headerd === undefined) return null ;
-
-    const contentTYpe = /(?<inputname>[^\/]+\/[^;$\s]+)/.exec(headerd)?.groups.inputname ;
-    return contentTYpe;
-}
-
-
