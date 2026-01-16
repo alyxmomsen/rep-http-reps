25efb8f2ec0a287c7f0e149895a11cd82f537c1e
daemon69 <myowngin@yandex.ru>
2026-01-16 15:47:19 +0300

    r8 :: form data handler

---
+++ router/handers/form-handlers/multiparthanlder.js
@@ -0,0 +1,170 @@
+const _log = require("../../../global-utils/log");
+
+
+async function multipartdatahandler(res , databuffer , boundarybuffer) {
+    
+    const formDataParts = await _splitBuffer(databuffer , boundarybuffer);
+
+    for (const part of formDataParts) {
+
+        await _handlePart(part);
+    } 
+
+    console.log('multipart resolver' , {formDataParts});
+    res.end('multipart resolver');
+    return ;
+}
+
+module.exports = multipartdatahandler ;
+
+async function _handlePart (partBuffer) {
+    
+    const splitedPartLike = await _splitPart(partBuffer);
+    
+    if(splitedPartLike === null) {
+        return ;
+    }
+    
+    const {body: bodyBuffer , headers} = splitedPartLike ;
+        
+    const {contentType: contentTypeString , filename: filenameString , name: nameString} = await _parsePartHeaders(headers.toString('utf-8'));
+
+    console.log({
+        contentType: contentTypeString , 
+        filename: filenameString , 
+        name: nameString , 
+        body: bodyBuffer
+    });
+    
+}
+
+async function _parsePartHeaders(dataHeadersString) {
+    
+    const rows = dataHeadersString.split('\r\n');
+
+    const headers = {} ;
+
+    rows.forEach(row => {
+
+        const [key , value] = row.split(': ');
+
+        if(key !== undefined && value !== undefined) {
+
+            headers[key.toLowerCase()] = value ;
+        }
+
+    });
+
+    const contentDispositionDataHeaderLike = headers['content-disposition'];
+    const contentTypeDataHeader = headers['content-type'];
+
+    const {name , filename} = await parseContentDisposition(contentDispositionDataHeaderLike) ;
+
+    return {
+        name , 
+        filename ,
+        contentType:contentTypeDataHeader || null ,
+    }
+
+    async function parseContentDisposition(contentDispositionStringLike) {
+
+        let name = null ;
+        let filename = null ;
+
+        if(contentDispositionStringLike !== undefined) {
+            
+            const namematch = contentDispositionStringLike.match(/name="([^"]+)"/);
+            const filenamematch = contentDispositionStringLike.match(/filename="([^"]+)"/);
+
+            if(namematch !== null) {
+
+                name = namematch[1] ;
+            }
+
+            if(filenamematch !== null) {
+
+                filename = filenamematch[1] ;
+            }
+        }
+
+        return {
+            name , 
+            filename ,
+        }
+    }
+}
+
+async function _splitPart(partBuffer) {
+    
+    const partSeparatorBuffer = Buffer.from('\r\n\r\n');
+
+    const partSeparatorBufferIndex = await _findBufferIndex(partBuffer , partSeparatorBuffer);
+
+    if(partSeparatorBufferIndex === -1) {
+        _log('incorrect data part'.toUpperCase());
+        return null;
+    }
+
+    const headersDataPartBuffer = partBuffer.subarray(0 , partSeparatorBufferIndex);
+    let bodyEndBufferIndex = partBuffer.length ;
+
+    if(partBuffer[partBuffer.length - 2] === 0x0d && partBuffer[partBuffer.length - 1] === 0x0a) {
+        bodyEndBufferIndex -= 2 ;
+    }
+
+    const bodyDataPartBuffer = partBuffer.subarray(
+        partSeparatorBufferIndex + partSeparatorBuffer.length , 
+        bodyEndBufferIndex
+    );
+
+    return {
+        headers:headersDataPartBuffer ,
+        body:bodyDataPartBuffer ,
+    }
+    
+}
+
+async function _splitBuffer(buffer , separator) {
+    
+    const parts = []; 
+
+    let start = 0 ;
+    let index = 0;
+
+    while ((index = await _findBufferIndex(buffer , separator , start)) !== -1) {
+
+        const foundPartBuffer = buffer.subarray(start , index);
+        parts.push(foundPartBuffer);
+        start = index + separator.length ;
+
+        if(buffer[start] === 0x0d && buffer[start + 1] === 0x0a) {
+            start += 2 ;
+        }
+    }
+
+    parts.push(buffer.subarray(start));
+
+    return parts ;
+}
+
+async function _findBufferIndex(buf , sep , start = 0) {
+
+    for (let ind = start ; ind < (buf.length - sep.length) ; ind++) {
+        let found = true ;
+
+        for (let j = 0 ; j < sep.length ; j++) {
+            
+            if(buf[ind + j] !== sep[j]) {
+                found = false; 
+                break;
+            }
+        }
+
+        if(found === true) {
+
+            return ind ;
+        }
+    }
+
+    return -1 ;
+}
--- router/handers/handle-form.js
+++ router/handers/handle-form.js
@@ -1,15 +1,74 @@
 const { createWriteStream } = require('fs');
 const { join } = require('path');
+const { ids } = require('webpack');
+const multipartdatahandler = require('./form-handlers/multiparthanlder');
+const _log = require('../../global-utils/log');
 async function handleForm(req ,res , storeAdapter) {
     
     const { headers } = req ;
+    const contentTypeHeader = headers['content-type'];
+
+    if(contentTypeHeader === undefined) {
+        console.log('no content type header');
+        res.end('no content type header');
+        return;
+    }
+
+    const [contentType , boundaryPartLike] = contentTypeHeader.split('; '); 
 
 
+    const bufferParts = [] ;
+    req.on('data' , async (chunk) => {
+        bufferParts.push(chunk);
+    });
 
-    console.log('handle form');
-    res.end('foobarbaz');
+    const contentTypeResolver = {
+        'multipart/form-data': {
+            handler: async () => {
+
+                if(boundaryPartLike === undefined) {
+                    _log('no boundary part');
+                    return ;
+                }
+
+                const boundaryMatch = boundaryPartLike.match(/boundary=(----[^;\s$]+)/);
+
+                if(boundaryMatch === null) {
+                    _log('no boundary');
+                    return;
+                }
+
+                const boundaryBuffer = Buffer.from(`--${boundaryMatch[1]}`);
+                _log({boundaryBuffer:boundaryBuffer.toString('utf-8')});
+                const wholeBuffer = Buffer.concat(bufferParts);
+
+                await multipartdatahandler(res ,wholeBuffer , boundaryBuffer);
+                
+            } ,
+        } ,
+        'other': {
+            
+        } ,
+    }
+    
+    req.on('end' , async () => {
+
+        console.log({contentTypeHeader});
+    
+
+        const contentTypeResolveLike = contentTypeResolver[contentType] ;
+
+        if(contentTypeResolveLike === undefined) {
+            console.log('sorry no resolver');
+            res.end('no resolver');
+            return;
+        }
+
+        await contentTypeResolveLike.handler(); 
+        
+        return ;
+    });
 }
 
-
-
 module.exports = handleForm ;
+
