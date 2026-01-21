1a03a285245449157baf2795991a8c399c26d712
daemon69 <myowngin@yandex.ru>
2026-01-22 01:00:26 +0300

    ref.complete-application.2026-01-19; define form data compiler

--- router/handlers/multipart-form-data/handle-multipart-data.js
+++ router/handlers/multipart-form-data/handle-multipart-data.js
@@ -1,6 +1,10 @@
 const getFilesFromStorage = require("../../../services/custom-store-manager/controller/get-files");
 const uploadFilesIntoStorage = require("../../../services/custom-store-manager/controller/upload-files-command");
+const FormDataCompiler = require("./services/form-data-compiler");
+const findBufferIndex = require("./utils/find-buffer-index");
 const _splitBufferBy = require("./utils/split-buffer-by-separator");
+
+const formDataCompiler = new FormDataCompiler();
 
 async function handleMultipartData (req , res , dataBuffer , boundaryStr = null) {
 
@@ -15,14 +19,32 @@ async function handleMultipartData (req , res , dataBuffer , boundaryStr = null)
     
     const parts  = await _splitBufferBy(dataBuffer , Buffer.from(boundaryStr));
 
-    uploadFilesIntoStorage();
-    getFilesFromStorage();
-
-    console.log({parts});
-
+    for (const part of parts) {
+        await handleFormPart(part);
+    }
+    
     res.end();
     return ;
 }
 
 module.exports = handleMultipartData ;
 
+// handle form data piece
+async function handleFormPart(formDataItemBuffer) {
+    
+    const separatorBuffer = Buffer.from(`\r\n\r\n`);
+
+    const separatorIndex = _splitBufferBy(formDataItemBuffer , separatorBuffer);
+
+    if(separatorIndex === -1) return console.log(`incorrect part`.toUpperCase());
+
+    const headersPartBuffer = formDataItemBuffer.subarray(0 , separatorIndex);
+    let bodyBufferEndIndex = formDataItemBuffer.length ;
+    if(formDataItemBuffer[bodyBufferEndIndex - 2] === 0x0d && formDataItemBuffer[bodyBufferEndIndex - 1] === 0x0a) {
+        bodyBufferEndIndex -= 2 ;
+    }
+
+    const bodyPartBuffer = formDataItemBuffer.subarray(separatorIndex + separatorBuffer.length , bodyBufferEndIndex);
+    
+    console.log({header:headersPartBuffer.toString('utf-8') , bodyPartBuffer});
+}
---
+++ router/handlers/multipart-form-data/services/form-data-compiler.js
@@ -0,0 +1,25 @@
+
+class FormDataCompiler {
+
+    // gulp one 
+    gulp () {
+
+    }
+
+    // pack these pieces
+    complete () {
+
+    }
+
+    #pieces;
+    #queue;
+    #files;
+
+    constructor () {
+        this.#pieces = [] ;
+        this.#files = {};
+        this.#queue = [];
+    }
+}
+
+module.exports = FormDataCompiler ;
