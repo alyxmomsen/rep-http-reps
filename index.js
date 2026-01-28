0282627bd7102bdab98596357fa551bc46381786
alex <shizofranky@gmail.com>
2026-01-28 15:58:14 +0300

    r4--router; init start

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const http = require('http');
 const router = require('./app/services/web-api/web-api');
 
-const server = http.createServer((req , res) => {
-    
-    router.handleRequest(req , res);
+const server = http.createServer((req, res) => {
+
+    router.handleRequest(req, res);
 });
 
 const port = 3333;
 const host = '0.0.0.0';
 
-server.listen(port, host , () => {
-    console.log({port , host});
+server.listen(port, host, () => {
+    console.log({ port, host });
 });
 
