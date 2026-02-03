930179f033d2b63f376e1a96bb97c07b9fd18181
daemon69 <myowngin@yandex.ru>
2026-02-03 12:49:59 +0300

    r16; pause

---
+++ app/services/router/router.js
@@ -0,0 +1,143 @@
+const {} = require('fs');
+class Router {
+
+    async handleRequest(req , res) {
+
+        const {url , rawQueryStringLike} = this.#splitURL(fullURL);
+
+        for (const [_ , routeBundle] of methodRoutes) {
+
+            const match = routeBundle.regex.exec(url);
+
+            if(!match) continue ;
+
+            // bundle params
+
+            const params = {} ;
+
+            routeBundle.keys.forEach((key , i) => {
+            
+                params[key] = match[i + 1] ;
+            });
+            
+            const queryParams = this.#extractQueryParams(rawQueryStringLike) ;
+
+            // -----------------------
+
+            // apply params to req object
+
+            req.params = params ;
+            req.queryParams = queryParams ;
+
+            // ------------------------------
+
+            await this.#executeMiddleware(req ,res , [...this.#middleware , routeBundle.middleware]);
+
+            routeBundle.handler(req , res);
+        }
+    }
+
+    use (...middleware) {
+        middleware.forEach(mw => {
+            this.#middleware.push(mw);
+        });
+    }
+
+    async #executeMiddleware (req, res , middleware) {
+        let index = 0 ;
+        const next = async () => {
+            const handlerLike = middleware[index++] ;
+            if(!handlerLike) return ;
+            await handlerLike(req, res , next);
+        }
+
+        await next();
+    }
+
+    #addRoute (template , method , handlers) {
+
+        const colorizeConsole = {
+            RED:'\x1b[31m' ,
+            GREEN:'\x1b[32m' ,
+            ORANGE:'\x1b[33',
+            DEF:'\x1b[0m' ,
+        }
+
+        const { url:fullURL} = req ;
+
+        const _method = method.toUpperCase(method);
+
+        const methodRoutes = this.#routes.get(_method);
+
+        if(!methodRoutes) {
+            throw new Error(`this method < ${_method} > is not accepted`);
+        }
+
+        if(methodRoutes.has(template)) {
+            throw new Error(`this template < ${template} > is already in use`);
+        }
+
+        const routeBundle = this.#compileRouteBundle(template , handlers);
+
+        methodRoutes.set(template , routeBundle);
+        
+        console.log(`${colorizeConsole.ORANGE}route ${_method} ${routeBundle.originalTemplate} just added${colorizeConsole.DEF}`);
+
+    }
+
+    #compileRouteBundle (template , handlers) {
+
+        return {
+            keys ,
+            handler , 
+            middleware ,
+            originalTemplate ,
+
+        }
+    }
+
+    #extractQueryParams (rawQueryStringLike) {
+
+        const params = {} ;
+
+        if(!rawQueryStringLike) return params ;
+        
+        const couples = rawQueryStringLike.split('&');
+
+        couples.forEach(couple => {
+
+            const [key , value] = couple.split('=');
+
+            if(key && value) {
+                params[key.toLowerCase()] = value ;
+            };
+        });
+
+        return params ; 
+    }
+
+    #splitURL (fullURL) {
+
+        const [_url , rawQueryStringLike] = fullURL.split('?');
+
+        return {
+            url:/.+\/$/.test(_url) ? _url.replace(/\/$/ , '') : _url ,
+            rawQueryStringLike,
+        }
+    }
+
+    #routes;
+    #middleware;
+
+    constructor () {
+
+        this.#routes = new Map() ;
+        this.#middleware = [] ;
+
+        const acceptedMethods = [
+            'get' , 'post'
+        ] ;
+    }
+}
+
+module.exports = Router ;
---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const httpserver = http.createServer((req , res) => {
+
+});
+
+const port = 3333;
+const host = '127.0.0.1';
+
+httpserver.listen(port , host , () => {
+    console.log({port , host});
+});
