b61a7372c00987ec20f7210c2f8a7db112a1a5eb
daemon69 <myowngin@yandex.ru>
2026-02-10 20:21:14 +0300

    r20; start dev the form handler

---
+++ app/utils/handlers/handle-form/handle-form.js
@@ -0,0 +1,12 @@
+const {} = require('');
+
+async function handleForm(req, res) {
+    
+    const { headers } = req ;
+
+    const contentType = headers['content-type'] ;
+
+    
+
+    res.end('foo bar');
+}
