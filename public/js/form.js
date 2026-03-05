024c986de437387be8384ecf049fa6b8586945d5
daemon69 <myowngin@yandex.ru>
2026-03-05 21:06:44 +0300

    update frontent: add playlist new form item

--- public/js/form.js
+++ public/js/form.js
@@ -12,10 +12,18 @@ window.addEventListener('DOMContentLoaded' , () => {
     const mainForm = document.getElementById('form--main');
     const statusBar = document.getElementById('status--upload');
     const modalWindow  = new ModalWindow(document.getElementById('modal-window--main'));
+    const addElementButton  = document.getElementById('button--add-element');
+
+    addElementButton.addEventListener("click" , (e) => {
+        e.stopPropagation();
+
+        new PlaylistFormElement('hello world' , 'foo-bar').insertInto(mainForm);
+
+    });
 
     mainForm.addEventListener("submit" , async (e) => {
         e.preventDefault();
-
+        
         console.log(e.currentTarget === mainForm);
 
         // hide modal window
@@ -201,3 +209,98 @@ class ModalWindow {
 
     }
 }
+
+class PlaylistFormElement {
+
+    /**
+     * 
+     * @param {HTMLElement} nestElement 
+     */
+    insertInto(nestElement) {
+        nestElement.appendChild(this.#baseElement);
+    }
+
+    #baseElement;
+
+    constructor (innerText , tablename) {
+
+        this.#baseElement = document.createElement('div');
+
+        const bEl = this.#baseElement ;
+        bEl.className = 'flex flex--col flex--jtf-ctr flex--align-start flex--gap-1 form-element' ;
+
+        // constants:
+
+        const INPUT_TYPES = {
+            TEXT:'text', 
+            FILE:'file', 
+        }
+
+        const GROUP_DATA_BASE = {
+            GROUP_ID:generateRandomString(32) ,
+            TABLE_NAME:tablename ,
+        }
+
+        const COLUMN_NAMES_KEYS = {
+            TITLE:'title', 
+            DESCRIPTION:'description', 
+        }
+
+        const COLUMN_DATA_TYPES = {
+            STRING:'string',
+            BINARY:'binary',
+        }
+
+        const { GROUP_ID , TABLE_NAME } = GROUP_DATA_BASE ;
+        const { TITLE , DESCRIPTION } = COLUMN_NAMES_KEYS ;
+        const { STRING , BINARY } = COLUMN_DATA_TYPES ;
+
+        // utils: 
+
+        const createNameAttr = (groupid , tablename ,colname , datatype) => {
+            return `${groupid}.${tablename}.${colname}.${datatype}` ;
+        }
+
+        // inners:
+
+        const h3 = document.createElement('h3');
+        h3.innerText = innerText ;
+
+        const titleInput = document.createElement('input');
+        titleInput.type = INPUT_TYPES.TEXT ;
+        titleInput.name = createNameAttr(GROUP_ID , TABLE_NAME , TITLE , STRING) ;
+
+        const descriptionInput = document.createElement('input');
+        descriptionInput.type = INPUT_TYPES.TEXT ;
+        descriptionInput.name = createNameAttr(GROUP_ID , TABLE_NAME , DESCRIPTION, STRING);
+
+        const fileInput = document.createElement('input');
+        fileInput.type = INPUT_TYPES.FILE ;
+        fileInput.name = createNameAttr(GROUP_ID , TABLE_NAME , 'video', BINARY);
+        
+        bEl.appendChild(h3);
+        bEl.appendChild(titleInput);
+        bEl.appendChild(descriptionInput);
+        bEl.appendChild(fileInput);
+    }
+}
+
+/* 
+<div class="flex flex--col flex--jtf-ctr flex--align-start flex--gap-1 form-element">
+    <h3>playlist element</h3>
+    <input type="text" name="G22.playlist-1.title.string" id="">
+    <input type="text" name="G22.playlist-1.description.string" id="">
+    <input type="file" name="G22.playlist-1.video-min.binary" id="" accept=".mkv, .mp4">
+</div>
+*/
+
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
+
