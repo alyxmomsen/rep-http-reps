3d3c29bd4968fe696f0e78dccca9b7dc4952c175
daemon69 <myowngin@yandex.ru>
2026-03-12 12:30:14 +0300

    refactor http controller

--- app/services/router/controller/http-controller.js
+++ app/services/router/controller/http-controller.js
@@ -7,34 +7,37 @@ const Router = require("../router");
 
 const router = new Router();
 
-// this first
+/* get static files */
 router.get('/static/*', async (req, res) => await handleStatic(req, res));
-
-router.get('/foo/*/bar', (req, res) => {
-    res.end('test');
-});
-
-router.get('/foo/bar/*', (req, res) => {
-    res.end('test');
-});
-
-router.get('/app', async (req, res) => handleReactApp(req , res));
-router.get('/api/videos', async (req, res) => {
-    
-});
-
-router.get('/video/:filename' , async (req  ,res) => {});
-
-router.get('/l/form', renderMultipartForm);
-
-router.post('/api/handle-form', FormHandler.processForm );
-
+/* public data route */
 router.get('/public/:type/:id', async (req ,res) => await handlePublic(req , res) ) ;
 
-// test route
+/* react-app view */
+router.get('/app', async (req, res) => handleReactApp(req , res));
+
+/* get playlist */
+router.get('/api/videos', async (req, res) => {res.end()});
+/* get video-stream */
+router.get('/video/:filename' , async (req  ,res) => {});
+
+/* view */
+router.get('/l/form', renderMultipartForm);
+
+/* обработчик-роутер для всех зарегестрированных form content-type данных   */
+router.post('/api/handle-form', FormHandler.processForm );
+
+/* test route for URL params */
 router.get('/test/:id/foo/:bar' , async (req  , res) => {
     const { method , url , params , queryParams } = req ;
     res.end(JSON.stringify({method , url , params , queryParams }));
+});
+/* test route for any tails */
+router.get('/foo/bar/*', (req, res) => {
+    res.end('test');
+});
+/* test route for any pathes in the middle of the URL */
+router.get('/foo/*/bar', (req, res) => {
+    res.end('test');
 });
 
 module.exports = { router }
