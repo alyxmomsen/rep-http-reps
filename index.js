32c0525978116e95aae6fd428ddba21ce10d0074
daemon69 <myowngin@yandex.ru>
2026-04-28 16:14:14 +0300

    update index.js

--- index.js
+++ index.js
@@ -7,6 +7,76 @@ const httpServer = http.createServer(async (req, res) => {
     await router.handleRequest(req, res);
 });
 
-httpServer.listen(3333, '0.0.0.0', () => {
-    console.log('hello bro');
-});
+const startServer = async () => {
+    await printGreatings()();
+    httpServer.listen(3333, '0.0.0.0', async () => {
+        console.log(`server started`);
+    });
+};
+
+startServer();
+
+/**
+ *
+ * @param {Object} deps
+ * @returns {() => Promise<void>}
+ */
+function printGreatings(deps = {}) {
+    const fn = async function () {
+        return new Promise(async (resolve, reject) => {
+            const messageParts = [
+                `welcome to the Knight Bus!`,
+                `Emergency transport for stranded witch or wizzard.`,
+                `My name is Stan Shunpike,`,
+                `and i\`ll be you conductor for this evening`,
+            ];
+
+            const message = messageParts.join(`\r\n`);
+
+            // console.log(`\x1b[38;2;255;0;255m` + message + `\x1b[0m`);
+
+            const TimoutBuffer = {
+                id: Infinity,
+                duration: 1000,
+                messageLength: 0,
+                appearMessage: '',
+            };
+
+            const print = async function () {
+                if (++TimoutBuffer.messageLength <= message.length) {
+                    TimoutBuffer.appearMessage = message.slice(
+                        0,
+                        TimoutBuffer.messageLength
+                    );
+                } else {
+                    if (TimoutBuffer.id !== Infinity) {
+                        clearTimeout(TimoutBuffer.id);
+                    }
+
+                    return resolve();
+                }
+                console.clear();
+                console.log(
+                    `\x1b[38;2;` +
+                        `${Math.floor(Math.random() * 255)};` +
+                        `${Math.floor(Math.random() * 255)};` +
+                        `${Math.floor(Math.random() * 255)}m` +
+                        TimoutBuffer.appearMessage +
+                        `\x1b[0m`
+                );
+
+                if (TimoutBuffer.id !== Infinity) {
+                    clearTimeout(TimoutBuffer.id);
+                }
+                TimoutBuffer.id = setTimeout(
+                    print,
+                    Math.floor(Math.random() * 200)
+                );
+            };
+
+            await print();
+        });
+    };
+
+    return fn;
+}
